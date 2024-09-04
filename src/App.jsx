0e727522d6de9ec4d067370b3fef69e153c04b45
Fleege <76089935+pfleege@users.noreply.github.com>
2024-09-04 12:40:48 +0300

    ADD accessor functions for x and y + scale data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { extent } from "d3";
 import useData from "./assets/useData";
 
 function App() {
@@ -7,6 +8,18 @@ function App() {
   if (!data) {
     return <pre>Loading data...</pre>;
   }
+
+  const xValue = (d) => d.sepal_length;
+  const yValue = (d) => d.sepal_width;
+
+  const xScale = scaleLinear()
+    .domain(extent(data, xValue))
+    .range([0, innerWidth]);
+
+  const yScale = scaleLinear()
+    .domain(extent(data, yValue))
+    .range([0, innerWidth]);
+
   return (
     <>
       <pre>The data has been loaded!</pre>
