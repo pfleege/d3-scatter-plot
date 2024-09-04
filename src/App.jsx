aeee881d4910e52583dad3471dba12ccad8b8704
Fleege <76089935+pfleege@users.noreply.github.com>
2024-09-04 17:42:39 +0300

    ADD styling and fine tune graph layout

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,10 @@ import AxisLeft from "./assets/AxisLeft";
 
 const width = 960;
 const height = 500;
-const margin = { top: 20, right: 20, bottom: 20, left: 200 };
+const margin = { top: 20, right: 20, bottom: 70, left: 200 };
 const innerWidth = width - margin.right - margin.left;
 const innerHeight = height - margin.top - margin.bottom;
+const yAxisLabelOffset = 45;
 
 function App() {
   const data = useData();
@@ -19,11 +20,14 @@ function App() {
   }
 
   const xValue = (d) => d.sepal_length;
+  const xAxisLabel = "Sepal Length";
   const yValue = (d) => d.sepal_width;
+  const yAxisLabel = "Sepal Width";
 
   const xScale = scaleLinear()
     .domain(extent(data, xValue))
-    .range([0, innerWidth]);
+    .range([0, innerWidth])
+    .nice();
 
   const yScale = scaleLinear()
     .domain(extent(data, yValue))
@@ -33,7 +37,26 @@ function App() {
     <svg height={height} width={width}>
       <g transform={`translate(${margin.left}, ${margin.top})`}>
         <AxisBottom xScale={xScale} innerHeight={innerHeight} />
+        <text
+          className="axis-label"
+          x={innerWidth / 2}
+          y={innerHeight + 50}
+          textAnchor="middle"
+          fill="white"
+        >
+          {xAxisLabel}
+        </text>
         <AxisLeft yScale={yScale} innerWidth={innerWidth} />
+        <text
+          className="axis-label"
+          transform={`translate(${-yAxisLabelOffset},${
+            innerHeight / 2
+          }) rotate(-90)`}
+          textAnchor="middle"
+          fill="white"
+        >
+          {yAxisLabel}
+        </text>
         <DataMarks
           data={data}
           xScale={xScale}
--- src/assets/AxisLeft.jsx
+++ src/assets/AxisLeft.jsx
@@ -2,7 +2,7 @@ const AxisLeft = ({ yScale, innerWidth }) => {
   return yScale.ticks().map((tick, i) => (
     <g key={i} transform={`translate(0, ${yScale(tick)})`}>
       {<line x2={innerWidth} stroke="white" />}
-      <text dy="2em" x={-6} style={{ textAnchor: "end", stroke: "white" }}>
+      <text dy={5} x={-6} style={{ textAnchor: "end", stroke: "white" }}>
         {tick}
       </text>
     </g>
