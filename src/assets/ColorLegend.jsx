8e884d721068d41c2d5575ee98145abf3ab33ca9
Fleege <76089935+pfleege@users.noreply.github.com>
2024-09-14 12:52:48 +0300

    ADD event listener to ColorLegend -module
    - Event listener will be used to have the data marks stand out in the
    graph when you hover over one of the spieces in the color legend

--- src/assets/ColorLegend.jsx
+++ src/assets/ColorLegend.jsx
@@ -5,7 +5,11 @@ const ColorLegend = ({
   tickTextOffsetX = 20,
 }) =>
   colorScale.domain().map((domainValue, i) => (
-    <g key={domainValue} transform={`translate(0,${i * legendSpacing})`}>
+    <g
+      key={domainValue}
+      transform={`translate(0,${i * legendSpacing})`}
+      onMouseOver={() => console.log("Hello!")}
+    >
       <circle r={tickSize} fill={colorScale(domainValue)}></circle>
       <text x={tickTextOffsetX} dy="0.32em" fill="white">
         {domainValue}
